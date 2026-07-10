a01f9dd74bfd2e9c948326ea60d5e1111dd727dc
sinaaghli <sina.aghli@gmail.com>
2026-07-09 21:13:59 -0600

    docs home update

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -185,7 +185,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Lynx Technologies. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Lynx Technologies.`,
     },
     prism: {
       theme: prismThemes.vsLight,
